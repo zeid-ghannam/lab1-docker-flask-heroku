814d237d3db5a1f013a142f38c0f0ee4baffe8df
ziedg <zeid.w.ghannam@gmail.com>
2024-10-06 16:13:44 +0300

    use remote db instead of local

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # if local
-until pg_isready -h db -U admin -d distlab; do
+# until pg_isready -h db -U admin -d distlab; do
 # if remote
-# until pg_isready -h dpg-cs0il423esus739369m0-a.frankfurt-postgres.render.com -U persona_id_user -d persona_id; do
+until pg_isready -h dpg-cs0il423esus739369m0-a.frankfurt-postgres.render.com -U persona_id_user -d persona_id; do
   echo "Waiting for database to be ready..."
   sleep 2
 done
